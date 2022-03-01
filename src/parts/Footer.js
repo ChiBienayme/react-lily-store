90ccfbd10392ebbdd1267b07908ef5f5ee305eab
Ha Chi HOANG <chibienayme@gmail.com>
2022-03-01 17:32:10 +0100

    Add Footer

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 // CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-export default function Footer () {
-  
-    return (
-      <div>
-          <code className='text-dark position-absolute top-100 start-50 translate-middle'>
-            Created by  
-            <a href="https://github.com/ChiBienayme/react-lily-store.git" target="none">
-                 Ha Chi HOANG
-            </a>
-              by using React - 25/02/2022 
-          </code>
-      </div>
-    )
-  
+export default function Footer() {
+  return (
+    <div>
+      <code className="text-dark position-absolute top-100 start-50 translate-middle">
+        Created by
+        <a
+          href="https://github.com/ChiBienayme/react-lily-store.git"
+          target="none"
+        >
+          <span> Ha Chi HOANG </span>
+        </a>
+        by using React - 25/02/2022
+      </code>
+    </div>
+  );
 }
-
